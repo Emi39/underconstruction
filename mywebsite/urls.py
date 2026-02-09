0c283f4b45e27be93c10ec64c20e8a033b4d2bba
popovychvanja <85509224+popovychvanja@users.noreply.github.com>
2026-02-09 13:57:01 -0600

    Add SEO files

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 def home(request):
     return render(request, 'index.html')
@@ -8,4 +9,6 @@ def home(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+    path('sitemap.xml', TemplateView.as_view(template_name="sitemap.xml", content_type="application/xml")),
 ]
